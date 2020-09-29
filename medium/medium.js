3340a6fd595ee60d2c6f0c4f80c943b50e3620ff
Pete Looney <petelooney81@gmail.com>
2020-09-29 11:19:02 -0500

    medium 3

--- medium/medium.js
+++ medium/medium.js
@@ -2,26 +2,44 @@
     // program that prompts the user for a domain name, looks up the IP address for the domain, and prints it out.
 const dns = require('dns');
 const readline = require('readline');
-const { add } = require('../log');
 
-let domainName;
+// let domainName;
 
-// Readline portion to get a user input
-const rl = readline.createInterface({
-    input: process.stdin,
+// // Readline portion to get a user input
+// const domainQuestion = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// domainQuestion.question('What domain name would you like to search? \n', (answer)=>{
+//     domainName = answer;
+//     // DNS lookup portion
+//     dns.lookup(domainName, (err, address, family)=>{
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log('Domain Name: '+domainName+'\nIP Address: '+ address + '\nFamily: ' + family);
+//         }
+//     });
+//     domainQuestion.close();
+// })
+
+// 2 Read a file
+
+const fs = require('fs');
+
+const anotherQuestion = readline.createInterface({
+    input : process.stdin,
     output: process.stdout
-});
-
-rl.question('What domain name would you like to search? \n', (answer)=>{
-    domainName = answer;
-    // DNS lookup portion
-    dns.lookup(domainName, (err, address, family)=>{
-        if(err){
-            console.log(err);
-        }else{
-            console.log('Domain Name: '+domainName+'\nAddress: '+ address + '\nFamily: ' + family);
-        }
-    });
-    rl.close();
 })
+
+anotherQuestion.question('What file would you like to read? \n', (fileName) =>{
+    fs.readFile(fileName, 'utf8', (err, data)=>{
+        if(err) throw err;
+        console.log(data);
+    })
+    anotherQuestion.close();
+})
+
+// 3 read and write to a file
 
