8b0af7c426ece0e68405bac7c177503115fc167f
Pete Looney <petelooney81@gmail.com>
2020-09-29 16:46:50 -0500

    large number 9 done prt 2

--- large/juggling-async.js
+++ large/juggling-async.js
@@ -3,7 +3,6 @@ const urls = [process.argv[2], process.argv[3], process.argv[4]];
 
 function printResults(dataArray){
     if(dataArray.length === 3){
-        // console.log(dataArray.url, dataArray[index]. dataArray.length);
         for(let index = 0; index < urls.length; index++){
             for (let innerIndex = 0; innerIndex < dataArray.length; innerIndex++){
                 if(dataArray[innerIndex].url === urls[index]){
