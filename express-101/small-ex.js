917ccd8cca95dcdf154825456935e3871d950efc
Pete Looney <petelooney81@gmail.com>
2020-09-30 14:49:16 -0500

    small exercises

---
+++ express-101/small-ex.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+const server = http.createServer(app);
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello World</h1>');
+})
+
+app.get('/greet/:name', (req, res)=>{
+    const {name} = req.params;
+
+    res.send(`<h1>Hello, ${name}!</h1>`)
+})
+
+app.get('/:animals', (req, res)=>{
+    const {animals} = req.params
+    let returnStatement='';
+    if(animals === 'cats'){
+        returnStatement = 'Meow!';
+    }else if(animals === 'dogs'){
+        returnStatement = 'Woof!';
+    }else if(animals === 'cats_and_dogs'){
+        returnStatement = 'Dogs and cats living together...mass hysteria!!';
+    }else(
+        returnStatement = 'Oops'
+    )
+    res.send(`<h1>${returnStatement}</h1>`);
+
+})
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}/`);
+});
