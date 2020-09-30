4ed3caa84b7009c835507694a4ad5f8c945befc3
Pete Looney <petelooney81@gmail.com>
2020-09-29 23:15:08 -0500

    added expressway.js

---
+++ express-101/expressWay.js
@@ -0,0 +1,26 @@
+// also requires http
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express(); // call the express function to create a new app.
+
+const server = http.createServer(app);
+
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+})
+
+app.get('/favicon.ico', (req, res)=>{
+    res.send('');
+})
+
+app.get('*', (req, res)=>{
+    res.send('This is not the home page')
+})
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}/`);
+});
