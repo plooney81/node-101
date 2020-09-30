5af638fa50115e618edf13abec5c4d45cae1c98b
Pete Looney <petelooney81@gmail.com>
2020-09-30 11:14:07 -0500

    refactor babysteps

--- large/baby-steps.js
+++ large/baby-steps.js
@@ -1,7 +1,7 @@
 // console.log(process.argv[2]);
 let holdSum = 0; 
 for (let index = 2; index < process.argv.length; index++){
-    holdSum += parseInt(process.argv[index]);
+    holdSum += parseInt(process.argv[index], 10);
 }
 
 console.log(holdSum);
